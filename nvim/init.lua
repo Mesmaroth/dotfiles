d1a211809e18436cbd03964c933e3d3b47c7e9bd
Robert Sandoval <mesmaroth@gmail.com>
2025-02-08 11:56:15 -0800

    Neovim dot files

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+require("settings")
+require("plugins")
+require("maps")
+
+
+-- colorscheme config: kanagawa
+local themeStatus, kanagawa = pcall(require, "kanagawa")
+
+if themeStatus then
+  vim.cmd("colorscheme kanagawa")
+else
+  return
+end
+
+
