65103a026a5e59dd48d32e2b23280b9ece5a90ac
Robert Sandoval <mesmaroth@gmail.com>
2026-02-07 14:18:09 -0800

    Added ghostty config Updated neovim configs Updated tmux conf

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("v", "=", function()
+  vim.lsp.buf.format({
+    async = false,
+    range = {
+      ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
+      ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
+    },
+  })
+end, { desc = "Format selection with LSP" })
+
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,29 @@
 return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "adwaita",
-    },
-  },
-
-  {
     "Mofiqul/adwaita.nvim",
     lazy = false,
+    priority = 1000,
     config = function()
       vim.g.adwaita_darker = true
-      vim.g.adwaita_transparent = false
+      vim.g.adwaita_disable_cursorline = false
+      vim.g.adwaita_transparent = true
     end,
+  },
+  { "dotsilas/darcubox-nvim" },
+  { "yashguptaz/calvera-dark.nvim" },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "night-owl",
+    },
   },
 }
